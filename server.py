3209b90aae9045ec2078e22c6e0b9cb64bf2e447
Jeremy Nelson <jermnelson@gmail.com>
2011-12-23 12:59:09 -0700

    Midday commit, worked on UI and MARCtoFRBR parser

--- server.py
+++ server.py
@@ -4,10 +4,15 @@
 
 from bottle import debug,get,post,request,route,run,static_file,template
 import redis,json,logging
-import config,sys
+import os,config,sys
 from lib import common,dc,isbd,frbr,marc21
+from mako.template import Template
+from mako.lookup import TemplateLookup
+
 
 debug(True)
+templates_lookup = TemplateLookup(directories=[os.path.join(os.path.dirname(__file__),
+                                                            'views')])
 
 @get('/')
 def index():
@@ -31,7 +36,7 @@ def index():
 def initialize():
     """
     Calls load_rdf methods in modules. Should only be run once
-    or if you to clear the datastore.
+    or if you want to clear the datastore.
     """
     common.redis_server.flushdb()
     isbd.load_forms()
@@ -45,6 +50,39 @@ def server_static():
     return static_file('index.html', root='doc/_build/html/')
 
 
+@route('/js/:filename')
+def send_static_js(filename):
+    root = "%s/views/js" % os.getcwd()
+    return static_file(filename,
+                       root=root)
+
+@route('/css/:filename')
+def send_static_js(filename):
+    root = "%s/views/css" % os.getcwd()
+    #"/home/jpnelson/frbr-redis-datastore/views/css"
+    return static_file(filename,
+                       root=root)
+
+
+@route('/visualizations/index.html')
+def visualizations():
+    """
+    Function displays FRBR-Redis Visualizations page
+    """
+    visualization_template = templates_lookup.get_template('visualization.html')
+    output = visualization_template.render(active_page='visual')
+    return output
+
+@route('/about.html')
+def about():
+    """
+    Function displays FRBR-Redis About page
+    """
+    about_template = templates_lookup.get_template('about.html')
+    return about_template.render(active_page='about')
+
+    
+
 @post('/:redis_key/add')
 def add_frbr_entity(redis_key):
     """
@@ -56,19 +94,19 @@ def add_frbr_entity(redis_key):
     else:
         return json.dumps({'uid':entity_ID})
 
-@get('/:redis_key/:uid')
-def frbr_entity(redis_key,uid=None):
-    """
-    Attempts to retrieve the FRBR entity with a Redis key and a 
-    unique id from FRBR Redis datastore.
-
-    :param redis_key: Redis Key
-    :param uid: Unique REDIS key for FRBR work
-    """
-    entity = frbr.get_entity(redis_key,uid)
-    if entity is None:
-        return json.dumps(None)
-    else:
-        return json.dumps(entity)
+##@get('/:redis_key/:uid')
+##def frbr_entity(redis_key,uid=None):
+##    """
+##    Attempts to retrieve the FRBR entity with a Redis key and a 
+##    unique id from FRBR Redis datastore.
+##
+##    :param redis_key: Redis Key
+##    :param uid: Unique REDIS key for FRBR work
+##    """
+##    entity = frbr.get_entity(redis_key,uid)
+##    if entity is None:
+##        return json.dumps(None)
+##    else:
+##        return json.dumps(entity)
 
 run(host=config.WEB_HOST,port=config.WEB_PORT)
