e768649cecdeffaecc3d1fa91a08b255e5abaed3
Jeremy Nelson <jermnelson@gmail.com>
2012-02-27 21:50:57 +0000

    Simplified development of commands, continued UI work on Call Number App

--- aristotle/apps/call_number/commands.py
+++ aristotle/apps/call_number/commands.py
@@ -31,32 +31,17 @@ def get_callnumber(record):
 def get_previous(call_number):
     current_rank = redis_server.zrank('call-number-sort-set',call_number)
     entities = []
-    previous_callnumbers = redis_server.zrange('call-number-sort-set',current_rank-2,current_rank-1)
-    for number in previous_callnumbers:
-        entities.append(record(number))
-    return entities    
+    return get_slice(current_rank,current_rank-2,current_rank-1)
 
 def get_next(call_number):
     current_rank = redis_server.zrank('call-number-sort-set',call_number)
+    return get_slice(current_rank,current_rank+1,current_rank+2)
+
+def get_slice(current_rank,start,stop):
     entities = []
-    next_callnumbers = redis_server.zrange('call-number-sort-set',current_rank+1,current_rank+2)
+    call_number_slice = redis_server.zrange('call-number-sort-set',start,stop)
     for number in next_callnumbers:
         entities.append(record(number))
-    return entities    
-
-
-def old_get_slice(start,end):
-    entities = []
-    positions = redis_server.lrange('call_numbers',
-                                    start,
-                                    end)
-    for redis_id in positions:
-        entity = redis_server.hgetall("record:%s" % redis_id)
-        if entity['author'] == 'None':
-            entity['author'] = None
-        else:
-            entity['author'] = [entity['author'],]
-        entities.append(entity)
     return entities
 
 
@@ -86,26 +71,6 @@ def ingest_records(marc_file_location):
     for record in marc_reader:
         ingest_record(record)
     
-
-def old_ingest_records(marc_file):
-    marc_reader = pymarc.MARCReader(open(marc_file,"rb"))
-    for record in marc_reader:
-                call_number = get_callnumber(record)
-        if call_number is None:
-            pass
-        else:
-                        redis_server.rpush("all_records",redis_id)
-            redis_server.sort("all_records",
-                      by="record:*->call_number",
-                      alpha=True,
-                      store="call_numbers")
-    sorted_callnumbers = redis_server.lrange('call_numbers',0,-1)
-    for i,redis_id in enumerate(sorted_callnumbers):
-        redis_server.hset("record:%s" % redis_id,
-                          "sort-position",
-                          i)
-
-
 def record(call_number):
     record_key = redis_server.hget('call-number-hash',call_number)
     return redis_server.hgetall(record_key)
