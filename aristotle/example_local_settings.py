fb1cd273d12896196720204ea77506d94a9aa41a
Jeremy Nelson <jeremy.nelson@coloradocollege.edu>
2012-03-02 16:04:10 -0700

    Added new Budget App

---
+++ aristotle/example_local_settings.py
@@ -0,0 +1,4 @@
+REDIS_HOST = 'solr.coloradocollege.edu'
+REDIS_PORT = 6379
+CALL_NUMBER_DB = 4
+DISCOVERY_RECORD_URL = 'http://discovery.coloradocollege.edu/catalog/record' 
--- aristotle/settings.py
+++ aristotle/settings.py
@@ -7,7 +7,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Jeremy Nelson', 'jeremy.nelson@coloradocollege.edu'),
 )
 
 MANAGERS = ADMINS
