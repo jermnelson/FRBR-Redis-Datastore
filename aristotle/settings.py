87f90845cf817c94f7fa8e024ad85acabdad2634
dwesterfield <dwesterfield@TuttLibSys.coloradocollege.edu>
2012-03-02 16:04:13 -0700

    Add Test app

--- aristotle/settings.py
+++ aristotle/settings.py
@@ -7,7 +7,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    # ('Diane Westerfield', 'diane.westerfield@coloradocollege.edu'),
 )
 
 MANAGERS = ADMINS
