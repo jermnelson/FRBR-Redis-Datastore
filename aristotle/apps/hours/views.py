a19760fff02f21383296b127a680452dc927dac4
jdriscoll <jdriscoll@TuttLibSys.coloradocollege.edu>
2012-03-09 15:35:30 -0700

    Continued development of Hours app

--- aristotle/apps/hours/views.py
+++ aristotle/apps/hours/views.py
@@ -1,1 +1,17 @@
-# Create your views here.
+"""
+
+ :mod: views Hours App Views
+"""
+__author__ = "Jon Driscoll"
+
+from django.views.generic.simple import direct_to_template
+
+def default(request):
+    """
+    default is the standard view for the Hours app
+    
+    :param request: web request
+    """
+    return direct_to_template(request,
+                               'hours/app.html',
+                               {})
