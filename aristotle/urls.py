91f22ebc4c9eb356aa464b9ec8bcb9eebaabe340
dwesterfield <dwesterfield@TuttLibSys.coloradocollege.edu>
2012-03-09 15:36:22 -0700

    Continued development of database app

--- aristotle/urls.py
+++ aristotle/urls.py
@@ -10,7 +10,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^call_number/', include('call_number.urls')),
-    url(r'^portfolio/', include('portfolio.urls')), 
+    url(r'^portfolio/', include('portfolio.urls')),
+    url(r'^database/', include('database.urls')), 
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
