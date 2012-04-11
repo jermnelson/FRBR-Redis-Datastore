2fa07214d27937152a9648efce5007624e10fa41
Jon Driscoll <jdriscoll@coloradocollege.edu>
2012-04-11 15:49:18 -0600

    began development of book_search widget

--- aristotle/apps/book_search/urls.py
+++ aristotle/apps/book_search/urls.py
@@ -7,5 +7,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('book_search.views',
     url(r"^$","default",name='book_search-app-default'),
+    url(r'widget$','widget'),
 )
                        
