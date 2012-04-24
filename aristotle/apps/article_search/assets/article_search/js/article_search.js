d4ad6bbeefa68c50365ac9531d29c8cec1765b50
Gautam <gwebb@TuttLibSys.coloradocollege.edu>
2012-04-24 16:43:03 -0600

    Completed external javascript files for Book and Article Search Apps.

--- aristotle/apps/article_search/assets/article_search/js/article_search.js
+++ aristotle/apps/article_search/assets/article_search/js/article_search.js
@@ -1,10 +1,10 @@
 function ChangeSearchArticlesText(btn_text) {
- $('#dbs').attr('html',btn_text);
+ $("#dbs").html(btn_text);
 }
 
 function OnSubmitForm() {
- var search_url;
- var search_char = $('#dbs').val().charAt(7);
+ var search_url = "";
+ var search_char = $("#dbs").html().charAt(7);
  switch(search_char) {
   case 'A':
    search_url = search_url.concat("http://0-search.ebscohost.com.tiger.coloradocollege.edu:80/login.aspx%3Fdirect=true&db=a9h&bquery=", escape($('#searchIcon').val()).replace(/%20/g, '+'), "&type=1&site=ehost-live");
@@ -15,6 +15,6 @@ function OnSubmitForm() {
   case 'J':
    search_url = search_url.concat("http://0-www.jstor.org.tiger.coloradocollege.edu/action/doBasicSearch%3FQuery=", escape($('#searchIcon').val()).replace(/%20/g, '+'), "&wc=on&acc=on");
  }
- $('#searchForm').action = url;
+ $("#searchForm").attr("action", search_url);
  return true;
 }
--- aristotle/apps/book_search/assets/book_search/js/book_search.js
+++ aristotle/apps/book_search/assets/book_search/js/book_search.js
@@ -1,17 +1,21 @@
 function ChangeSearchBookText(btn_text) {
- $('#searchby').attr('html',btn_text);
- $('#searchby').val(btn_text);
+ $("#searchby").html(btn_text);
+ $("#searchby").val(btn_text);
  var search_char = btn_text.charAt(7);
  switch(search_char) {
   case 'A':
    $('#search-type').val("author_search");
    break;
   case 'J':
-   ${'#search-type').val("title_search");
+   $('#search-type').val("title_search");
+   break;
   case 'K':
-   ${'#search-type').val("search");
+   $('#search-type').val("search");
+   break;
   case 'S':
-   ${'#search-type').val("subject_search");
+   $('#search-type').val("subject_search");
+   break;
   case 'T':
-   ${'#search-type').val("title_search");
+   $('#search-type').val("title_search");
+ }
 }
