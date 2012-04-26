701b35d22b62d2ed103a96262681a9622427a5ec
Gautam <gwebb@TuttLibSys.coloradocollege.edu>
2012-04-26 15:58:26 -0600

    Eliminated repetition of code.

--- aristotle/apps/article_search/assets/article_search/js/article_search.js
+++ aristotle/apps/article_search/assets/article_search/js/article_search.js
@@ -7,13 +7,13 @@ function OnSubmitForm() {
  var search_char = $("#dbs").html().charAt(7);
  switch(search_char) {
   case 'A':
-   search_url = search_url.concat("http://0-search.ebscohost.com.tiger.coloradocollege.edu:80/login.aspx%3Fdirect=true&db=a9h&bquery=", escape($('#searchIcon').val()).replace(/%20/g, '+'), "&type=1&site=ehost-live");
+   search_url = search_url.concat("http://0-search.ebscohost.com.tiger.coloradocollege.edu:80/login.aspx%3Fdirect=true&db=a9h&bquery=", escape($('#searchArticleIcon').val()).replace(/%20/g, '+'), "&type=1&site=ehost-live");
    break;
   case 'G':
-   search_url = search_url.concat("http://0-scholar.google.com.tiger.coloradocollege.edu/scholar%3Fq=", escape($('#searchIcon').val()).replace(/%20/g, '+'));
+   search_url = search_url.concat("http://0-scholar.google.com.tiger.coloradocollege.edu/scholar%3Fq=", escape($('#searchArticleIcon').val()).replace(/%20/g, '+'));
    break;
   case 'J':
-   search_url = search_url.concat("http://0-www.jstor.org.tiger.coloradocollege.edu/action/doBasicSearch%3FQuery=", escape($('#searchIcon').val()).replace(/%20/g, '+'), "&wc=on&acc=on");
+   search_url = search_url.concat("http://0-www.jstor.org.tiger.coloradocollege.edu/action/doBasicSearch%3FQuery=", escape($('#searchArticleIcon').val()).replace(/%20/g, '+'), "&wc=on&acc=on");
  }
  $("#searchForm").attr("action", search_url);
  return true;
