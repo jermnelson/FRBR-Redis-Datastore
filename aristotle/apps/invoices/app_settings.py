97698084937e9e3d13a8b9d78badbd97d997a779
Jeremy Nelson <jeremy.nelson@coloradocollege.edu>
2012-04-25 21:47:00 -0600

    Conintued to work on Invoice App

--- aristotle/apps/invoices/app_settings.py
+++ aristotle/apps/invoices/app_settings.py
@@ -1,5 +1,5 @@
-APP = {'current_view': {'title':'Budget App'},
-       'description': 'The Budget App is an administrative productivity app for managing money associated with Library.',
+APP = {'current_view': {'title':'Invoice App'},
+       'description': 'The Invoice App is an administrative access & productivity app for invoices associated with Library Operations',
        'icon_url':'budget.png',
-       'url':'budget/'}
+       'url':'invoices/'}
        
