08209e795db658b741bf87336fe07477d213c590
Jeremy Nelson <jeremy.nelson@coloradocollege.edu>
2012-05-01 10:18:34 -0600

    Adding ILS, Redis, and Solr jobs to th MARC batch app

---
+++ aristotle/apps/marc_batch/jobs/ils.py
@@ -0,0 +1,8 @@
+"""
+ :mod:`ils` Legacy ILS MARC Batch jobs. This module is the latest iteration
+ of the work being done in the Tutt Library cataloging department. We are
+ trying to simplify and automate the MARC record manipulation we do to 
+ MARC records we receive from our various database vendors.
+"""
+__author__ = 'Jeremy Nelson'
+
